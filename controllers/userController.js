14a6e5102cf459759341fac9eed7ab72c0a3c762
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2024-09-26 23:37:00 +0530

    Refactoring The File System 📂

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({ status: 'Error', message: 'Under Maintainence' });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({ status: 'Error', message: 'Under Maintainence' });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({ status: 'Error', message: 'Under Maintainence' });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({ status: 'Error', message: 'Under Maintainence' });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({ status: 'Error', message: 'Under Maintainence' });
+};
