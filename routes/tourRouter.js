3b235bec70b0800f5b58c34058912c2d7148db2d
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2024-09-27 23:08:48 +0530

    Implemented Param Router

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -9,6 +9,11 @@ const {
 } = require('../controllers/tourController');
 const router = express.Router();
 
+router.param('id',(req,res,next , val)=>{
+  console.log(`Tour ID : ${val}`);
+  next();
+})
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
