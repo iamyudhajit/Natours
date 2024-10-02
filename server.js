3fa42a56cf010b3d81479a94d824be0cc2943887
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2024-10-02 14:25:34 +0530

    Connecting Mongoose & Mongo

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+// console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+const tourScehma = new mongoose.Schema({
+  name: { type: String, required: true },
+  rating: Number,
+  price: Number,
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
